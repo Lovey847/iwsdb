3dd44b8012c640d39f6bcab9efb2accb2b86e6dd
Lovey847 <lovey847@gmail.com>
2025-05-07 10:18:59 -0500

    Add a lot more documentation to apple_main.mm

--- src/plat/apple_main.mm
+++ src/plat/apple_main.mm
@@ -45,17 +45,24 @@
 
 #import <Cocoa/Cocoa.h>
 
-// Reference to the view in 
+// Global reference to window's view
 static apple_view_t *s_view;
+
+// If false, the mainloop stops
 static bfast s_running = true;
 
+// Timer frequency, also used in apple_audio.mm
 timestamp_t g_timerFrequency = 0;
+
+// Global format string buffer used by FMT
 char g_fmtStr[FMTSTR_SIZE];
 
+// Convert timestamp to microseconds
 static inline u32 TimeToMicro(timestamp_t time) {
   return time*1000000/g_timerFrequency;
 }
 
+// Move rectangle to center of screen
 static void CenterRect(NSRect *rect) {
   const NSRect scrFrame = [[NSScreen mainScreen] frame];
 
@@ -65,6 +72,7 @@ static void CenterRect(NSRect *rect) {
     scrFrame.origin.y + (scrFrame.size.height - rect->size.height) * 0.5;
 }
 
+// Create and assign app menu
 static void CreateMenu() {
   NSMenu *mainMenu;
   NSMenuItem *menuItem;
@@ -94,11 +102,14 @@ static void CreateMenu() {
   [NSApp setMainMenu:mainMenu];
 }
 
+// Creates a window using apple_view_t for the view, handles initialization
+// and destruction of the game and all interfaces, and handles window events.
 @interface iwsdb_t : NSObject<NSApplicationDelegate, NSWindowDelegate> {
   NSWindow *win;
   apple_view_t *view;
 }
 
+// s_view assigned to view in main
 @property (nonatomic,weak,readwrite) apple_view_t *view;
 
 @end    //@interface iwsdb_t
@@ -107,6 +118,7 @@ static void CreateMenu() {
 
 @synthesize view = view;
 
+// Create window and its view
 - (id)init {
   self = [super init];
   if (self) {
@@ -124,6 +136,8 @@ static void CreateMenu() {
                                     backing:NSBackingStoreBuffered
                                       defer:NO];
     [win setTitle:@"I wanna slay the dragon of bangan"];
+
+    // iwsdb_t handles window events
     [win setDelegate:self];
 
     view =
@@ -151,6 +165,7 @@ static void CreateMenu() {
                           cStringUsingEncoding:NSUTF8StringEncoding];
   chdir(dataDir);
 
+  // Initialize interfaces
   AllocMem();
   InitTimer();
   InitLogStreams();
@@ -168,17 +183,24 @@ static void CreateMenu() {
     LOG_ERROR(FMT.s("\"").s(dataDir).s("/data\" isn't a directory!").STR);
   }
 
+  // On mac, this just initializes the renderer.
   CreateWindow(NULL, "I wanna slay the dragon of bangan");
+
   InitAudio();
+
+  // Initialize the game
   InitGame();
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification*)notif {
   CreateMenu();
+
+  // Make widow visible
   [win makeKeyAndOrderFront:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification*)notif {
+  // Write save at end of execution, if possible
   stream_t saveFile = {};
   if (g_state->save.valid() &&
       OpenFile(&saveFile, "save.dat", FILE_WRITE_ONLY))
@@ -190,11 +212,18 @@ static void CreateMenu() {
     LOG_INFO("== Unable to write save data! ==");
   }
 
+  // Free game
   FreeGame();
+
+  // Free interfaces
   FreeAudio();
+
+  // On mac, this just shuts down the renderer.
   CloseWindow(NULL);
+
   CloseLogStreams();
 
+  // Indicate to the mainloop that the application is done running
   s_running = false;
 }
 
@@ -241,20 +270,27 @@ void AppleAlert(const char *msg) {
 
 int main() {
   @autoreleasepool {
+    // Create NSApp
     [NSApplication sharedApplication];
 
+    // Initialize app delegate
     iwsdb_t *app = [[[iwsdb_t alloc] init] autorelease];
     [NSApp setDelegate:app];
 
+    // Initialize global reference to view
     s_view = app.view;
 
+    // Request app focus
     [NSApp activate];
 
+    // Initialize parts of the app before the mainloop
     [NSApp finishLaunching];
 
     do {
+      // Save time of the start of the frame, in microseconds
       u32 start = TimeToMicro(GetTime());
 
+      // Poll events
       NSEvent *evt;
       @autoreleasepool {
         for (;;) {
@@ -269,18 +305,25 @@ int main() {
         }
       }
 
+      // Update which keys are held down in the view
       [s_view UpdateDown];
+      // Update the game
       UpdateGame([s_view GetInput]);
+      // Update shift key state in the view
       [s_view UpdateShift];
+      // Render view's contents
       [s_view Render];
+      // Update audio mixer
       UpdateAudio();
 
+      // Delay until the time the next frame should start
       u32 end = TimeToMicro(GetTime());
       if (end - start < 1000000 / GAME_FPS) {
         MicrosecondDelay(g_timerFrequency, 1000000 / GAME_FPS - (end - start));
       }
     } while (s_running);
 
+    // Remove reference to app.view
     s_view = nil;
   }
 
